0b50d985f4ba755c8d9f1928b0867771a16795e4
fconejer <javier.conejero@bsc.es>
2019-06-13 17:40:19 +0200

    Matmul_objects cleaned. Pep8. python 2 and 3 compatible. Fixed its readme file.

--- python/matmul_objects/src/matmul_objects.py
+++ python/matmul_objects/src/matmul_objects.py
@@ -17,51 +17,40 @@
 
 # -*- coding: utf-8 -*-
 
-def initialize_variables():
-    import numpy as np
-    for matrix in [A,B,C]:
-        for i in range(MSIZE):
-            matrix.append([])
-            for j in range(MSIZE):
-                if matrix == C:
-                    block = np.array(np.zeros((BSIZE, BSIZE)), dtype=np.double, copy=False)
-                else:
-                    #aux = np.zeros((BSIZE, BSIZE))
-                    #aux.fill(2.0)
-                    #block = np.array(aux, dtype=np.double, copy=False)
-                    block = np.array(np.random.random((BSIZE, BSIZE)), dtype=np.double, copy=False)
-                mb = np.matrix(block, dtype=np.double, copy=False)
-                matrix[i].append(mb)
-
-
-### TASK SELECTION ###
+import sys
+import numpy as np
 
 from pycompss.api.task import task
 from pycompss.api.parameter import *
 
-@task(c = INOUT)
-def multiply(a, b, c):
-    #import time
-    #start = time.time()
 
+def initialize_variables():
     import numpy as np
-    #np.show_config()
+    for matrix in [A, B, C]:
+        for i in range(MSIZE):
+            matrix.append([])
+            for j in range(MSIZE):
+                if matrix == C:
+                    block = np.array(np.zeros((BSIZE, BSIZE)),
+                                     dtype=np.double, copy=False)
+                else:
+                    block = np.array(np.random.random((BSIZE, BSIZE)),
+                                     dtype=np.double, copy=False)
+                mb = np.matrix(block, dtype=np.double, copy=False)
+                matrix[i].append(mb)
 
+
+# ## TASK SELECTION ## #
+
+@task(c=INOUT)
+def multiply(a, b, c):
+    import numpy as np
     c += a*b
 
-    #end = time.time()
-    #tm = end - start
-    #print "TIME: " + str(tm*1000) + " msec"
 
+# ## MAIN PROGRAM ## #
 
-
-### MAIN PROGRAM ###
 if __name__ == "__main__":
-
-    import sys
-    import numpy as np
-
-    np.show_config()
 
     args = sys.argv[1:]
 
@@ -79,6 +68,6 @@ if __name__ == "__main__":
             for k in range(MSIZE):
                 multiply(A[i][k], B[k][j], C[i][j])
 
-#    for i in range(MSIZE):
-#        for j in range(MSIZE):
-#           print "C" + str(i) + str(j) + "=" + str(compss_wait_on(C[i][j]))
+    # for i in range(MSIZE):
+    #     for j in range(MSIZE):
+    #         print "C" + str(i) + str(j) + "=" + str(compss_wait_on(C[i][j]))
