2f4d53536a05f1f4e86c48aa7de5e7a525e02ad5
fconejer <javier.conejero@bsc.es>
2019-06-13 16:16:23 +0200

    Added missing header.

--- python/hands-on/src/generator.py
+++ python/hands-on/src/generator.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+#
+#  Copyright 2002-2019 Barcelona Supercomputing Center (www.bsc.es)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# -*- coding: utf-8 -*-
+
 """ Words generator """
 import sys
 import os
@@ -21,4 +40,3 @@ if __name__ == "__main__":
                 f.write(paragraph[2])
                 sizeText = os.path.getsize(path)
         print "{} generated: size {} mb".format(fileName, (float(sizeText)/1024.0)/1024.0)
-
