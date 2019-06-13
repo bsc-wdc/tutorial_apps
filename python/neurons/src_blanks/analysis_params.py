f2220a7dee63db37c509aeb43789a387702cb1d1
fconejer <javier.conejero@bsc.es>
2019-06-13 17:59:43 +0200

    Updated license headers of neurons.

--- python/neurons/src_blanks/analysis_params.py
+++ python/neurons/src_blanks/analysis_params.py
@@ -1,21 +1,31 @@
+# -*- coding: utf-8 -*-
 #
-#  Copyright 2002-2015 Barcelona Supercomputing Center (www.bsc.es)
+# Code for:
+# Chapter 20: Practically Trivial Parallel Data Processing in a Neuroscience Laboratory
+# M. Denker, B. Wiebelt, D. Fliegner, M. Diesmann, A. Morrison
+# In: Analaysis of Parallel Spike Trains (2010) S. Gruen and S. Rotter (eds). Springer Series in Computational Neuroscience 7
+# http://www.spiketrain-analysis.org
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Analysis-related parameters
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Copyright 2010 Michael Denker, B. Wiebelt, D. Fliegner, M. Diesmann, A. Morrison
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 maxlag = 200
-num_surrs = 200
-#num_surrs = 1000
+#num_surrs = 600
+num_surrs = 1000
 #num_surrs = 20 
 
